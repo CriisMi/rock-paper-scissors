13a19d5cf9864f64766c407d60a1737d210f699e
Cris <cristinamihalciuc@yahoo.com>
2022-06-01 19:54:37 +0300

    Add game function
    Play game of 5 rounds. Get imput from user and compare it with result from computerPlay. Keep track of player points and computer points and decide who wins at the end.

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function computerPlay() {
     }
 }
 
+/* decides who won from two paper scissors rock values*/
+
 function playRound(playerSelection, computerSelection) {
     let upperPlayer = playerSelection.toUpperCase();
     let upperComputer = computerSelection.toUpperCase();
@@ -25,5 +27,37 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-let player = 'Rock';
-console.log(playRound(player, computerPlay()));
+/* plays a game of 3 rounds and decides who wins the game */
+
+function game() {
+    let playerPoints = 0;
+    let computerPoints = 0;
+    for (i = 0; i < 5; i++) {
+        console.log(`Round ${i + 1}!`)
+        
+        let player = prompt('Choose: ');
+        console.log(`You chose ${player}`);
+
+        let computer = computerPlay();
+        console.log(`The computer chose ${computer}`);
+
+        let result = playRound(player, computer);
+        console.log(result);
+        if (result == 'You won!') {
+            playerPoints++;
+        } else if (result == 'You lost') {
+            computerPoints++;
+        }
+        console.log(`You: ${playerPoints} - Computer: ${computerPoints}`);
+    }
+
+    if (playerPoints > computerPoints) {
+        console.log(`You won the game!`)
+    } else if (computerPoints > playerPoints) {
+        console.log('The computer won the game')
+    } else {
+        console.log('This game is a tie');
+    }
+}
+
+game();

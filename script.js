ac14cd5a9538be0087bcabf449fb2d13eb24fc81
Cris <cristinamihalciuc@yahoo.com>
2022-06-01 18:32:30 +0300

    Create function computerPlay  that returns 'rock', 'paper' or 'scissors'

---
+++ script.js
@@ -0,0 +1,11 @@
+function computerPlay() {
+    let i = Math.floor(Math.random() * 3);
+    if (i == 0) {
+        return 'Rock';
+    } else if ( i == 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+

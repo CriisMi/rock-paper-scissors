c2e949e6926133b77e68a5fbeb620d7fdd3ceda7
Cris <cristinamihalciuc@yahoo.com>
2022-06-01 18:52:19 +0300

    Add function playRound that decides who wins

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+/* generate rock paper or scissors*/
+
 function computerPlay() {
     let i = Math.floor(Math.random() * 3);
     if (i == 0) {
@@ -9,3 +12,18 @@ function computerPlay() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    let upperPlayer = playerSelection.toUpperCase();
+    let upperComputer = computerSelection.toUpperCase();
+
+    if (upperPlayer == upperComputer) {
+        return 'It\'s a tie';
+    } else if (upperPlayer == 'ROCK' && upperComputer == 'SCISSORS' || upperPlayer == 'SCISSORS' && upperComputer == 'PAPER' || upperPlayer == 'PAPER' && upperComputer == 'ROCK') {    
+        return 'You won!';
+    } else {
+        return 'You lost';
+    }
+}
+
+let player = 'Rock';
+console.log(playRound(player, computerPlay()));
